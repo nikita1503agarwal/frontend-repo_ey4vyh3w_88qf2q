d14b717045df897d82b8b3dbbd3b0e1d81ba8db2
Flames <vibe-coder@flames.com>
2025-11-08 17:47:11 +0000

    Update project prj_ey4vyh3w - 2025-11-08T17:47:10.496Z

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,16 @@
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
-import About from './components/About'
 import Projects from './components/Projects'
-import Skills from './components/Skills'
-import Teaching from './components/Teaching'
 import Contact from './components/Contact'
-import SEO from './components/SEO'
-import Footer from './components/Footer'
 
 function App() {
   return (
-    <div className="font-inter bg-white dark:bg-black text-black dark:text-white scroll-smooth">
-      <SEO />
+    <div className="font-inter bg-white dark:bg-black text-black dark:text-white scroll-smooth min-h-screen">
       <Navbar />
       <main>
         <Hero />
-        <About />
         <Projects />
-        <Skills />
-        <Teaching />
         <Contact />
-        <Footer />
       </main>
     </div>
   )
